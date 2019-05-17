902754446acd3a2b76432312f4dd415719f9b3e4
Huma Tariq <humatariq773@gmail.com>
2019-05-18 00:08:31 +0500

    Add files via upload

---
+++ built-in functions.py
@@ -0,0 +1,20 @@
+language= input("Input a letter to know its length")
+
+string=input("Enter a num which you want to convert into a string")
+integer=input("Enter a num you want to convert into a integer")
+floating=input("Enter a num you want to convert into a float number")
+Type=input("Enter anything to check its type")
+language=len(language)
+Type=type(Type)
+integer=int(integer)
+floating=float(floating)
+string=str(string)
+
+
+print(language)
+
+print(string)
+print(integer)
+print(floating)
+print(Type)
+
